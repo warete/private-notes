eb30f8be0aa444513b3762e241e5a3f1d9027970
Egor Borisovsky <egor@intervolga.ru>
2022-06-18 16:36:51 +0300

    Конфиг и подключение к монго

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_CONNECTION'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
